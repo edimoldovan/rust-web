2d1f4d15385064d5563cc62e690a703cdad3111f
ed <edimoldovan@gmail.com>
2022-09-17 18:58:16 +0200

    css bundling and static serving

--- README.md
+++ README.md
@@ -8,19 +8,20 @@ A simple and opinionated web framework mostly based on the `actix-web`, server s
 - watch and restart the server with `cargo watch -x run`
 - using `askama` as template engine
 - middlewares, starting with a request logger
+- App is easily serving static files from `public` folder like this: `/public/some.file` available on `/static/some.file` url.serve static files from `public`
 
 ## upcoming features
-- automatic page reload: ws https://actix.rs/docs/websockets/
+- automatic page reload: ws https://actix.rs/docs/websockets/ , https://crates.io/crates/actix-ws
 - better css tooling
 - session handling, start with cookies: https://docs.rs/actix-session/latest/actix_session/
 - server side inclundes
-- serve static files from `public` folder: https://actix.rs/docs/static-files/
+
 - minify css `brew install tdewolff/tap/minify`
 - parse and use `config.toml`: 
 - use javascript import maps installed with `npm` and configured in `config.toml`
 - automatic page reload with a simple socket signalling
 - JSON API exammples: https://auth0.com/blog/build-an-api-in-rust-with-jwt-authentication-using-actix-web/
-- JWT examples, both issuing a new token at login and reading it
+- JWT examples, both issuing a new token at login and reading it: https://github.com/Keats/jsonwebtoken 
 
 - session handling, start with cookies
 - server side inclundes
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use actix_settings::{ApplySettings as _, Settings, Mode};
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::{Condition, Compress, Logger};
+use actix_files as fs;
 use askama_actix::{Template};
+use std::env;
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use lightningcss::{
+  bundler::{Bundler, FileProvider},
+  stylesheet::{ParserOptions, PrinterOptions} //MinifyOptions
+};
+
 
 #[derive(Template)]
 #[template(path = "hello.html")]
@@ -25,12 +35,26 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
   let mut settings = Settings::parse_toml("./config.toml")
     .expect("Failed to parse `Settings` from config.toml");
 
   Settings::override_field_with_env_var(&mut settings.actix.hosts[0].port, "PORT")?;
 
   init_logger(&settings);
+
+  // maybe use later
+  println!("The current directory is {}", env::current_exe().unwrap().display());
+  // write bundled css
+  let fs = FileProvider::new();
+  let mut bundler = Bundler::new(&fs, None, ParserOptions::default());
+  let stylesheet = bundler.bundle(Path::new("./assets/css/global.css")).unwrap();
+  // figure out minify later
+  // stylesheet.minify(MinifyOptions::default());
+  // println!("`{:?}`", stylesheet.minify(MinifyOptions::default()));
+  // println!("{}", stylesheet.to_css(PrinterOptions::default()).unwrap().code);
+  let mut file = File::create("./public/global.min.css")?;
+  write!(file, "{}", stylesheet.to_css(PrinterOptions::default()).unwrap().code)?;
 
   HttpServer::new({
     // clone settings into each worker thread
@@ -47,8 +71,10 @@ async fn main() -> std::io::Result<()> {
         .app_data(web::Data::new(settings.clone()))
         .service(hello)
         .service(echo)
+        // serve static files
+        .service(fs::Files::new("/static", "./public"))
         .route("/hey", web::get().to(manual_hello))
-    }   
+    }
   })
   .apply_settings(&settings)
   .run()
